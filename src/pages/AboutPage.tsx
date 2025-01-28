b36244c8eb5dc606bf778e2ed09b495d586eb2ae
petek <petek.metin@sabanciuniv.edu>
2025-01-28 14:36:53 +0300

    petek router ve hakkımızda

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,13 @@
+// src/pages/AboutPage.tsx
+import React from "react";
+
+const AboutPage: React.FC = () => {
+  return (
+    <div style={{ margin: "2rem" }}>
+      <h1>About Page</h1>
+      <p>add anything here</p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/pages/HakkimizdaPage.tsx
@@ -0,0 +1,21 @@
+
+import React from "react";
+import HeaderMenu from "../components/HeaderMenu";
+import Footer from "../components/Footer/Footer";
+
+
+const HakkimizdaPage: React.FC = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <HeaderMenu />
+      
+
+
+
+
+      <Footer />
+    </div>
+  );
+};
+
+export default HakkimizdaPage;
