a97ce418681549300b5ebdb81f5eac7b645c853f
aliz1369 <alirezazargarian@gmail.com>
2025-02-04 14:05:07 +0300

    fix main title

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,11 +1,15 @@
 // src/components/HeroSection.tsx
-import React, { useContext } from "react";
-import { LanguageContext } from "../contexts/LanguageContext";
-import translations from "../translations";
-
-const HeroSection: React.FC = () => {
-  const { language } = useContext(LanguageContext);
-  const t = translations[language];
+import React from "react";
+import { Component } from "../types/globalTypes";
+import StyledText from "./StyledText";
+// import { LanguageContext } from "../contexts/LanguageContext";
+// import translations from "../translations";
+interface HeroProps {
+  content: Component[] | null;
+}
+const HeroSection: React.FC<HeroProps> = ({ content }) => {
+  // const { language } = useContext(LanguageContext);
+  // const t = translations[language];
 
   return (
     <section className="relative w-full h-auto md:h-[57vh] overflow-hidden">
@@ -20,12 +24,20 @@ const HeroSection: React.FC = () => {
 
       {/* Metin İçerikleri */}
       <div className="relative z-10 flex flex-col items-start justify-center text-white h-full px-4 py-12 md:px-12 md:py-0">
-        <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-4 leading-tight">
-          {t.heroTitleLine1}
-          <br />
-          {t.heroTitleLine2}
-        </h1>
-        <p className="text-lg sm:text-xl md:text-2xl mb-4">{t.heroSubtitle}</p>
+        {content &&
+          content.map((data) =>
+            data.component_type === "title" ? (
+              <StyledText
+                content={data.content_value}
+                className="text-4xl sm:text-5xl md:text-6xl font-bold mb-4 leading-tight"
+              />
+            ) : (
+              <StyledText
+                content={data.content_value}
+                className="text-lg sm:text-xl md:text-2xl mb-4"
+              />
+            )
+          )}
       </div>
     </section>
   );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,10 @@
 // src/pages/MainPage.tsx
 import React, { useEffect, useState } from "react";
-// import HeroSection from "../components/HeroSection";
+import HeroSection from "../components/HeroSection";
 import PartnersReferences from "../components/PartnersReferences";
 import { fetchHomePage } from "../api/pageApi";
 import { Page } from "../types/globalTypes";
 import LoadingSpinner from "../components/LoadSpinner";
-import StyledText from "../components/StyledText";
 // import ChatBubble from "../components/ChatBubble";
 
 const MainPage: React.FC = () => {
@@ -32,30 +31,9 @@ const MainPage: React.FC = () => {
     <>
       <LoadingSpinner visible={loading} />
       <div className="flex flex-col min-h-screen">
-        {data?.sections.map((section, index) => (
-          <div
-            key={section.id}
-            className={`relative py-20 px-6 ${
-              index % 2 === 0
-                ? "bg-gradient-to-r from-blue-600 to-teal-500 text-white"
-                : "bg-white text-gray-900"
-            }`}
-          >
-            <div className="max-w-4xl mx-auto">
-              {section.components.map((component) => (
-                <div
-                  key={component.id}
-                  className={
-                    component.component_type === "title"
-                      ? "text-3xl font-bold text-white mb-4"
-                      : "text-lg text-gray-200 leading-relaxed"
-                  }
-                >
-                  <StyledText content={component.content_value} />
-                </div>
-              ))}
-            </div>
-            {/* <HeroSection /> */}
+        {data?.sections.map((section) => (
+          <div key={section.id}>
+            <HeroSection content={section.components} />
           </div>
         ))}
         <PartnersReferences />
