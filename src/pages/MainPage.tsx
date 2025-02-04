0b6787d4ef2f96ecb926eb52cc94ab9cf305f346
aliz1369 <alirezazargarian@gmail.com>
2025-02-04 13:37:57 +0300

    fix environment

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,38 +1,38 @@
 // src/pages/MainPage.tsx
-import React, { useEffect, useState } from "react";
+import React from "react";
 import HeroSection from "../components/HeroSection";
 import PartnersReferences from "../components/PartnersReferences";
-import { fetchHomePage } from "../api/pageApi";
-import { Page } from "../types/globalTypes";
-import LoadingSpinner from "../components/LoadSpinner";
-import StyledText from "../components/StyledText";
+// import { fetchHomePage } from "../api/pageApi";
+// import { Page } from "../types/globalTypes";
+// import LoadingSpinner from "../components/LoadSpinner";
+// import StyledText from "../components/StyledText";
 // import ChatBubble from "../components/ChatBubble";
 
 const MainPage: React.FC = () => {
-  const [data, setData] = useState<Page | null>(null);
-  const [loading, setLoading] = useState(false);
+  // const [data, setData] = useState<Page | null>(null);
+  // const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const data = async () => {
-      try {
-        setLoading(true);
-        const response = await fetchHomePage();
-        setData(response);
-      } catch (err) {
-        setLoading(false);
-        console.log(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-    data();
-  }, []);
+  // useEffect(() => {
+  //   const data = async () => {
+  //     try {
+  //       setLoading(true);
+  //       const response = await fetchHomePage();
+  //       setData(response);
+  //     } catch (err) {
+  //       setLoading(false);
+  //       console.log(err);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+  //   data();
+  // }, []);
 
   return (
     <>
-      <LoadingSpinner visible={loading} />
+      {/* <LoadingSpinner visible={loading} /> */}
       <div className="flex flex-col min-h-screen">
-        {data?.sections.map((section, index) => (
+        {/* {data?.sections.map((section, index) => (
           <div
             key={section.id}
             className={`relative py-20 px-6 ${
@@ -55,9 +55,9 @@ const MainPage: React.FC = () => {
                 </div>
               ))}
             </div>
-            <HeroSection />
           </div>
-        ))}
+        ))} */}
+        <HeroSection />
         <PartnersReferences />
         {/* <ChatBubble /> */}
       </div>
