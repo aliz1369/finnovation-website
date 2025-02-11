f8813861dd7ca833575bced162d0ca0e4796d0be
Mert Özen <mert.ozen@gmail.com>
2025-02-11 20:17:25 +0300

    surec izleme ve gelistirme sayfası guncellendi (#19)
    Co-authored-by: Mert_Ozen <mert_ozen@Mert-Ozens-MacBook-Pro.local>

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import AboutUsPage from "../pages/AboutUsPage";
+import SurecIzleme from "../pages/SurecIzleme"; // Süreç İzleme sayfasını import et
 
 // FinAcademy için eklediğimiz yeni sayfa
 import FinAcademyPage from "../pages/FinAcademyPage";
@@ -12,10 +13,10 @@ const AppRoutes: React.FC = () => {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/about" element={<AboutUsPage />} />
-
+        <Route path="/surec-izleme" element={<SurecIzleme />} />{" "}
+        {/* Yeni eklenen route */}
         {/* Yeni FinAcademy rotası */}
         <Route path="/academy" element={<FinAcademyPage />} />
-
         {/* Gerekiyorsa diğer sayfaları da ekleyebilirsiniz */}
       </Routes>
     </Router>
