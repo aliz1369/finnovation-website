e203806cdd2f16215461a940e9cd7b321b3e8060
Mert_Ozen <mert_ozen@Mert-Ozens-MacBook-Pro.local>
2025-02-11 19:12:15 +0300

    surec izleme ve gelistirme sayfası guncellendi

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import AboutUsPage from "../pages/AboutUsPage";
+import SurecIzleme from "../pages/SurecIzleme"; // Süreç İzleme sayfasını import et
 
 const AppRoutes: React.FC = () => {
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          {/* Diğer sayfaları buraya ekleyeceğiz */}
-          <Route path="/about" element={<AboutUsPage />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<AboutUsPage />} />
+        <Route path="/surec-izleme" element={<SurecIzleme />} /> {/* Yeni eklenen route */}
+      </Routes>
+    </Router>
   );
 };
 
 export default AppRoutes;
+
