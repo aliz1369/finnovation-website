2e5b8db78891981407034a10bb85aaf9111ea606
YusufAkyasann <yusufakyasan@gmail.com>
2025-02-11 22:33:05 +0300

    Merge conflicts ve route güncelleme fixi

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import AboutUsPage from "../pages/AboutUsPage";
+import HakkimizdaPage from "../pages/HakkimizdaPage";
 
 const AppRoutes: React.FC = () => {
   return (
       <Router>
         <Routes>
           <Route path="/" element={<MainPage />} />
-          {/* Diğer sayfaları buraya ekleyeceğiz */}
           <Route path="/about" element={<AboutUsPage />} />
+          <Route path="/hakkimizda" element={<HakkimizdaPage />} />
         </Routes>
       </Router>
   );
