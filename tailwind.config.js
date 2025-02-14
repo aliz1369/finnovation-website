be2a067b8e39aa6e2b604f0969c88d1b53e09911
Mert_Ozen <mert_ozen@Mert-Ozens-MacBook-Pro.local>
2025-02-14 11:17:13 +0300

    FinAcademy page is fixed and AccordionList component is added and fonts size are corrected

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,39 +4,56 @@ export default {
   theme: {
     extend: {
       spacing: {
-        '21': '5.25rem',  // Yaklaşık 84px
-        '22': '5.5rem',   // Yaklaşık 88px
-        '23': '5.75rem',  // Yaklaşık 92px
-        '24': '6rem',     // Yaklaşık 96px
-        '25': '6.25rem',  // Yaklaşık 100px
-        '26': '6.5rem',   // Yaklaşık 104px
-        '27': '6.75rem',  // Yaklaşık 108px
-        '28': '7rem',     // Yaklaşık 112px
-        '29': '7.25rem',  // Yaklaşık 116px
-        '30': '7.5rem',   // Yaklaşık 120px
+        21: "5.25rem", // Yaklaşık 84px
+        22: "5.5rem", // Yaklaşık 88px
+        23: "5.75rem", // Yaklaşık 92px
+        24: "6rem", // Yaklaşık 96px
+        25: "6.25rem", // Yaklaşık 100px
+        26: "6.5rem", // Yaklaşık 104px
+        27: "6.75rem", // Yaklaşık 108px
+        28: "7rem", // Yaklaşık 112px
+        29: "7.25rem", // Yaklaşık 116px
+        30: "7.5rem", // Yaklaşık 120px
+      },
+      fontSize: {
+        // Headings
+        h1: [
+          "5rem",
+          {
+            // 80px for web
+            lineHeight: "1.1",
+            letterSpacing: "-0.02em",
+            "@screen sm": {
+              fontSize: "2.5rem", // 40px for mobile
+            },
+          },
+        ],
+        h2: [
+          "2rem",
+          {
+            // 32px for web
+            lineHeight: "1.2",
+            letterSpacing: "-0.01em",
+            "@screen sm": {
+              fontSize: "1.5rem", // 24px for mobile
+            },
+          },
+        ],
+        // Paragraph
+        base: [
+          "1.125rem",
+          {
+            // 18px for web
+            lineHeight: "1.6",
+            "@screen sm": {
+              fontSize: "1rem", // 16px for mobile
+            },
+          },
+        ],
       },
       fontFamily: {
-        segouie: ['SegoeUI', 'sans-serif'],
+        segouie: ["SegoeUI", "sans-serif"],
       },
-      animation: {
-        'scroll': 'scroll 15s linear infinite',
-        'scroll-right': 'scrollRight 40s linear infinite',
-        'scroll-left': 'scrollLeft 40s linear infinite',
-      },
-      keyframes: {
-        scroll: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-100%)' }
-        },
-        scrollRight: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-100%)' }
-        },
-        scrollLeft: {
-          '0%': { transform: 'translateX(-100%)' },
-          '100%': { transform: 'translateX(0)' }
-        }
-      }
     },
   },
   plugins: [],
