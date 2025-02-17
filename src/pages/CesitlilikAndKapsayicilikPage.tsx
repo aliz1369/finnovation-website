726e384358a60bc018341f1c0531993d27cd167a
aliz1369 <alirezazargarian@gmail.com>
2025-02-17 16:05:13 +0300

    change allignment

--- src/pages/CesitlilikAndKapsayicilikPage.tsx
+++ src/pages/CesitlilikAndKapsayicilikPage.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import MainLayout from "../layouts/MainLayout";
-import HeroTitle from "../components/HeroTitle";
 import CallToAction from "../components/CallToAction";
+import HeroTitle from "../components/HeroTitle";
+import MainLayout from "../layouts/MainLayout";
 
 const CesitlilikAndKapsayicilikPage: React.FC = () => {
   return (
     <MainLayout>
       <div>
-        <section className="bg-white h-auto md:h-[calc(100vh-4rem)]">
+        <section className="bg-white h-auto ">
           <HeroTitle
             title="Her Ses, Her Renk, Birlikte Daha Güçlüyüz!"
             description=", çeşitliliği ve kapsayıcılığı temel değerleri arasında kabul eder. Farklı geçmişlerden, kültürlerden ve deneyimlerden gelen bireylerin bir araya geldiği dinamik bir çalışma ortamı yaratmak, inovasyonun en güçlü kaynağına inanıyoruz. Her bireyin eşit fırsatlara sahip olduğu, fikirlerin özgürce paylaşılabildiği ve tüm çalışanların kendilerini değerli hissettikleri bir ortam sağlamak için sürekli çaba gösteriyoruz. Çeşitli bakış açıları, yeni çözümler geliştirmemize yardımcı olur ve birlikte daha güçlü bir takım olmamızı sağlar. Finnovation'da herkes, kimliği ne olursa olsun, kendisini güvenli ve saygı gören bir ortamda bulur. Bu yaklaşımımız, sadece şirket içindeki ilişkilerimizi değil, aynı zamanda müşterilerimize sunduğumuz hizmetleri de iyileştirir."
@@ -21,4 +21,4 @@ const CesitlilikAndKapsayicilikPage: React.FC = () => {
   );
 };
 
-    export default CesitlilikAndKapsayicilikPage;
+export default CesitlilikAndKapsayicilikPage;
