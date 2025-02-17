f1f6e094f33761cf4458f522cb8bb6414b5d3694
YusufAkyasann <yusufakyasan@gmail.com>
2025-02-17 12:27:22 +0300

    En-Tr entegrasyonu

--- src/components/Footer/CTASection.tsx
+++ src/components/Footer/CTASection.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const CTASection: React.FC = () => {
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+
+  const handleClick = () => {
+    navigate('/contact');
+  };
+
   return (
     <section className="px-1 py-16 flex justify-center items-center">
       <div className="relative w-full max-w-6xl px-8 py-24 rounded-2xl shadow-xl overflow-hidden">
@@ -17,14 +26,17 @@ const CTASection: React.FC = () => {
 
         <div className="text-center relative z-10">
           <h2 className="text-5xl font-extrabold text-gray-900 leading-tight">
-           Yeni Projelere Birlikte İmza Atalım
+            {t("ctaTitle")}
           </h2>
           <p className="mt-4 text-lg text-gray-700">
-          Hemen bize ulaşın projelerinizi hayata geçirelim.
+            {t("ctaDescription")}
           </p>
           <div className="mt-8 flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-6">
-            <button className="px-8 py-4 bg-[#3277BC] text-white font-bold rounded-full shadow-lg hover:bg-[#285b8f] flex items-center transition-transform transform hover:scale-105">
-              Başvuru Yap
+            <button 
+              onClick={handleClick}
+              className="px-8 py-4 bg-[#3277BC] text-white font-bold rounded-full shadow-lg hover:bg-[#285b8f] flex items-center transition-transform transform hover:scale-105"
+            >
+              {t("ctaButton")}
               <span className="ml-2 text-xl">→</span>
             </button>
           </div>
