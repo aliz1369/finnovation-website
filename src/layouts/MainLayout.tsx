52dbb9543f7ac37126d603e388c2a7504a395877
Mert_Ozen <mert_ozen@Mert-Ozens-MacBook-Pro.local>
2025-02-17 17:02:10 +0300

    Cookie is added

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 // import Footer from "../components/Footer/Footer";
 import HeaderMenu from "../components/HeaderMenu";
+import { CookiesProvider, useCookies } from "react-cookie";
 
 interface MainLayoutProps {
   children: React.ReactNode;
@@ -9,17 +10,23 @@ interface MainLayoutProps {
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   const location = useLocation();
+  const [_, setCookie] = useCookies(["user"]);
 
   useEffect(() => {
+    setCookie("user", "gowtham", {
+      path: "/",
+    });
     window.scrollTo(0, 0);
   }, [location.pathname]); // location.pathname değiştiğinde tetiklenecek
 
   return (
-    <div className="min-h-screen w-full overflow-x-hidden">
-      <HeaderMenu />
-      <main className="pt-[80px]">{children}</main>
-      {/* <Footer /> */}
-    </div>
+    <CookiesProvider>
+      <div className="min-h-screen w-full overflow-x-hidden">
+        <HeaderMenu />
+        <main className="pt-[80px]">{children}</main>
+        {/* <Footer /> */}
+      </div>
+    </CookiesProvider>
   );
 };
 
