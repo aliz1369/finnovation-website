96f9b59dcadc453b783b6e685c8a38b54190c594
aliz1369 <alirezazargarian@gmail.com>
2025-02-19 15:11:53 +0300

    add some test to css

--- src/api/pageApi.ts
+++ src/api/pageApi.ts
@@ -9,33 +9,3 @@ export const fetchAboutPage = async () => {
   const response = await apiClient.get("/pages/about-us/");
   return response.data;
 };
-
-export const fetchdegerlerimizPage = async () => {
-  const response = await apiClient.get("/pages/degerlerimiz/");
-  return response.data;
-};
-
-export const fetchsurdurulebilirlikPage = async () => {
-  const response = await apiClient.get("/pages/surdurulebilirlik/");
-  return response.data;
-};
-
-export const fetchUzmanlikAlanlarimizPage = async () => {
-  const response = await apiClient.get("/pages/uzmanlik-alanlarimiz/");
-  return response.data;
-};
-
-export const fetchSurecIzlemePage = async () => {
-  const response = await apiClient.get("/pages/surec-izleme/");
-  return response.data;
-};
-
-export const fetchProjeBazliPage = async () => {
-  const response = await apiClient.get("/pages/proje-bazli-entegrasyon/");
-  return response.data;
-};
-
-export const fetchFinAcademyPage = async () => {
-  const response = await apiClient.get("/pages/finacademy/");
-  return response.data;
-};
